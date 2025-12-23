72e0df01632958de1b50572f6106c40bc4d63d38
saquino0827 <saquino@flexion.us>
2025-12-23 09:49:06 -0600

    Add report generation and the updated removal/addition reports

--- builder/src/generate-reports.ts
+++ builder/src/generate-reports.ts
@@ -4,6 +4,7 @@ import fs from "node:fs/promises";
 import axios from "axios";
 import {Readable} from "node:stream";
 import type {SiteScannerData} from "./types/site-scanner-data.js";
+import {RemovalData} from "./model/removal-data.js";
 
 async function fetchSiteScannerData() {
     console.log("Fetching site scanner data...");
@@ -39,8 +40,8 @@ async function fetchSiteScannerData() {
 }
 
 async function generateAdditions(siteScannerData: SiteScannerData[] | null) {
-    console.log("Filtering candidates for addition...");
     if (!siteScannerData) return;
+    console.log("Filtering candidates for addition...");
     let filteredScannerData =  siteScannerData.filter(row =>
         !row.source_list?.includes('omb_idea') &&
         row.branch === 'Executive' &&
@@ -57,16 +58,50 @@ async function generateAdditions(siteScannerData: SiteScannerData[] | null) {
         filteredScannerDataDf = filteredScannerDataDf.select("agency", "bureau", "initial_domain");
         let orderedFilteredScannerDataDf = filteredScannerDataDf.sortBy(['agency', 'bureau', 'initial_domain'])
         const writeCsv = orderedFilteredScannerDataDf.toCSV();
-        await fs.writeFile("../reports/test_candidates_for_addition.csv", writeCsv);
+        await fs.writeFile("../reports/candidates_for_addition.csv", writeCsv);
     } catch (error) {
         console.error(error);
     }
     console.log("Candidate for addition report has been generated.");
 }
 
+async function generateRemovals(siteScannerData: SiteScannerData[] | null) {
+    if (!siteScannerData) return;
+    console.log("Filtering candidates for removal...");
+    const filteredScannerData: RemovalData[] = [];
+    for (const siteScannerDataRow of siteScannerData) {
+        if (!siteScannerDataRow.source_list?.includes('omb_idea') &&
+            siteScannerDataRow.branch === 'Executive') {
+            let reason: string[] = [];
+            if (siteScannerDataRow.filter === 'false') reason.push('Filter');
+            if (siteScannerDataRow.redirect === 'true') reason.push('Redirect');
+            if (siteScannerDataRow.status_code !== undefined && Number(siteScannerDataRow.status_code) >= 400)
+                reason.push('Status Code');
+            if (reason.length > 0) {
+                const currentAgency = siteScannerDataRow.agency === undefined ? "" : siteScannerDataRow.agency;
+                const currentBureau = siteScannerDataRow.bureau === undefined ? "" : siteScannerDataRow.bureau;
+                const currentInitialDomain = siteScannerDataRow.initial_domain === undefined ? "" : siteScannerDataRow.initial_domain;
+                filteredScannerData.push(new RemovalData(currentAgency, currentBureau, currentInitialDomain, reason))
+            }
+        }
+    }
+    console.log(`Writing ${filteredScannerData.length} filtered candidates for removal...`);
+    if (filteredScannerData.length > 0) {
+        try {
+            let filteredScannerDataDf = new DataFrame(filteredScannerData);
+            let orderedFilteredScannerDataDf = filteredScannerDataDf.sortBy(['agency', 'bureau', 'initial_domain'])
+            const writeCsv = orderedFilteredScannerDataDf.toCSV();
+            await fs.writeFile("../reports/candidates_for_removal.csv", writeCsv);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
 export async function generateReports() {
     console.log("Running report generation...");
     const scannerApiData = await fetchSiteScannerData()
     await generateAdditions(scannerApiData);
+    await generateRemovals(scannerApiData);
     console.log("Report generation complete.");
 }
