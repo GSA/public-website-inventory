391f9c9caf9ed9e088412c7f505556007fce2807
saquino0827 <saquino@flexion.us>
2026-01-07 08:57:47 -0600

    Remove excess logging

--- builder/src/generate-inventory-stats.ts
+++ builder/src/generate-inventory-stats.ts
@@ -173,7 +173,6 @@ async function getInventoryAnalytics(row: PublicWebsiteInventory,
     const currentOffice = currentPublicInventoryRow.office;
     let currentInventoryAnalysis = new InventoryAnalysis(currentWebsite, currentAgency, currentBureau, currentOffice);
     const baseDomain = retrieveBaseDomainFromUrl(currentWebsite);
-    console.log(`Processing ${baseDomain}...`);
 
     for (const federalFileRow of federalFileData) {
         const currentFedRow = federalFileRow as FederalFileData;
